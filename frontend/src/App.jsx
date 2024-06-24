2bd4d3d44f9fa72d761cea451d4a669736803f6e
Jubair <jubair.ahmed2838@gmail.com>
2024-06-24 17:20:16 +0600

    :speech_balloon:[Update]: hero section text

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,19 @@ function App() {
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold ">Hello there</h1>
+          <div className="max-w-xl">
+            <h1 className="mb-5 text-5xl font-bold ">Hello, you lazy people</h1>
             <p className="mb-5">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered input-primary w-full max-w-lg"
+            />
+            <button className="btn btn-primary mt-4">Get Started</button>
           </div>
         </div>
       </div>
